dd0b76de785b4a1730326ec89f2681def03dd530
Dimpozd <dimpozd@yandex.ru>
2022-02-27 15:08:21 +0300

    Initial Commit

---
+++ Body Mass Index Calculator/Body_Mass_Index_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Body_Mass_Index_CalculatorApp.swift
+//  Body Mass Index Calculator
+//
+//  Created by Дмитрий Поздняков on 27.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Body_Mass_Index_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
